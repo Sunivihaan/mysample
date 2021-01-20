fd97a2c62a6ffb598944d00c2f4b56d41b5352a0
Suneetha <33000263+Sunivihaan@users.noreply.github.com>
2021-01-20 11:40:00 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,23 @@
-from flask import Flask
+import flask
+from flask import Flask ,request, jsonify
+from predict import *
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+	#print(os.environ)
+	return "Please Type a Message!"
 
+@application.route('/predict_msg', methods=['POST'])
+def predict_msg():
+    param = request.get_json(force=True)
+    msg = str(param['query'])
+    flag = str(predict_from_api(msg))
+    result = {"message":msg, "output":flag}
+    return jsonify(result)
+
+#checking in local environment
 if __name__ == "__main__":
-    application.run()
+	application.run()
+
